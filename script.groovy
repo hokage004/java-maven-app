5e9fc303531ea0526abca7f6130e43fb72f30d44
hokage004 <88645938+hokage004@users.noreply.github.com>
2026-09-26 15:18:08 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,17 +1,3 @@
-def buildJar() {
-  echo 'building the application...'
-  sh 'mvn package'
-}
-
-def buildImage() {
-   echo "building the docker image..."
-   withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-      sh 'docker build -t hokage004/demo-app:2.0 .'
-      sh 'docker login -u $USER -p $PASS'
-      sh 'docker push hokage004/demo-app:2.0'
-    }
-  }
-
 def deployApp() {
   echo 'deploying the application...'
 }
